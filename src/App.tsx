960c2f43534e5ed10d39d3138d53aa8e01c89492
unknown <brayancampa56@gmail.com>
2025-09-20 18:24:49 -0600

    funcionalidad de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
+import { CssBaseline } from '@mui/material'
 import './App.css'
+import { AppBarMenu } from './components/layout/AppBarMenu'
+import { useState } from 'react'
+import { SideNav } from './components/layout/SideNav'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { LoginForm } from './components/auth/LoginForm'
+import Swal from 'sweetalert2'
 
 function App() {
 
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handlerDrawerToggle = () => setDrawerOpen(!drawerOpen);
+
+const handlerLogout = () => {
+  Swal.fire({
+    title: "Logout",
+    text: "Estas seguro de cerrar sesion?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+        window.location.href = "/login";
+    }
+  });
+};
+
   return (
-    <>
-    kalum-app
-    </>
+    <Router>
+      <CssBaseline />
+      <AppBarMenu onMenuClick={handlerDrawerToggle} onLogout={handlerLogout}></AppBarMenu>
+      <SideNav open={drawerOpen} onClose={handlerDrawerToggle}></SideNav>
+      <Routes>
+        <Route path='/login' element={<LoginForm onLoginSuccess={() => window.location.href = 'wwww.google.com.gt'}/>}/>
+      </Routes>
+    </Router>
+
   )
 }
 
