d5d03681a8507d90e39fc24819182d98e634e0fe
unknown <brayancampa56@gmail.com>
2025-10-12 02:44:22 -0600

    Integration Api KalumAuth

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -8,15 +8,15 @@ interface LoginFormProps {
 }
 
 export const LoginForm: React.FC<LoginFormProps> = ({ onLoginSuccess }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('kalum-user');
+    const [password, setPassword] = useState('Inicio.2025');
     const [error, setError] = useState('');
     const { login } = useAuth();
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        login(email, password).then((token: any) => {
-            if (token) {
+        login(email, password).then((response: any) => {
+            if (response?.data?.token) {
                 Swal.fire({
                     title: 'Login',
                     text: 'Bienvenido al Sistema',
@@ -29,7 +29,7 @@ export const LoginForm: React.FC<LoginFormProps> = ({ onLoginSuccess }) => {
             } else {
                 Swal.fire({
                     title: 'Login',
-                    text: 'Usuario o contraseña incocorrectos, vuelva intentar',
+                    text: 'Usuario o contraseña incorrectos, vuelva intentar',
                     icon: 'error'
                 }).then((confirm) => {
                     if (confirm.isConfirmed) {
