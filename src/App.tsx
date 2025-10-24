48ce4d4a988173aab208c2c359c221c389de05f7
unknown <brayancampa56@gmail.com>
2025-10-23 20:32:24 -0600

    Component User finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Swal from 'sweetalert2';
 import { CareerList } from './components/careers/CareerList';
 import { useAuth } from './hooks/useAuth';
 import { ProtectedRoute } from './routes/ProtectedRoute';
+import { UserList } from './components/users/UserList';
 
 function App() {
   const { isAuthenticated, logout } = useAuth();
@@ -48,6 +49,11 @@ function App() {
             <CareerList />
           </ProtectedRoute>      
         } />
+        <Route path='/users' element= {
+          <ProtectedRoute>
+            <UserList/>
+          </ProtectedRoute>
+        }/>
         <Route path='/' element={<Navigate to="/careers" />} />
       </Routes>
     </Router>
