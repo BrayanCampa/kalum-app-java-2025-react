4cdcb9e49aff628fc33b6a13bbcff312351b340f
unknown <brayancampa56@gmail.com>
2026-01-24 15:07:47 -0600

    arreglar error del hooks

--- src/components/admissionExams/AdmissionExamList.tsx
+++ src/components/admissionExams/AdmissionExamList.tsx
@@ -159,7 +159,7 @@ export const AdmissionExamList = () => {
             jornadaId: academicDaySelected?.jornadaId,
             carreraId: careerSelected?.carreraId
         }
-        // Llamar Api
+        // Llamado a la Api
         Swal.fire({
             title: 'Solicitud examen de admision',
             text: 'Su solicitud fue enviada exitosamente, pronto recibira un correo con la información para finalizar el proceso',
